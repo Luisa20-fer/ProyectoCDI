3149d0827b70427843e7b42bdb542060dab16216
Luisafer18 <Luisafernnadezs2017@gmail.com>
2019-08-22 08:18:41 -0500

    Initial commit

---
+++ ProyectoCDI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProyectoCDI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
